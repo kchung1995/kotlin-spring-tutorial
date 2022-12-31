10eab614bf0774d4c7f771448073052666320834
Kuyho Chung <kchung1995@gmail.com>
2022-12-31 15:18:53 +0900

    Initial commit

---
+++ src/test/kotlin/com/tistory/katfun/blog/KopringTutorialApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tistory.katfun.blog
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringTutorialApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
