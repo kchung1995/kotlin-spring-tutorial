4fba9938d5a01a2a4e38267eb431dd29f0167248
Kuyho Chung <kchung1995@gmail.com>
2023-01-07 01:52:17 +0900

    updated IntegrationTests.kt

--- src/test/kotlin/com/tistory/katfun/blog/IntegrationTests.kt
+++ src/test/kotlin/com/tistory/katfun/blog/IntegrationTests.kt
@@ -20,14 +20,20 @@ class IntegrationTests(@Autowired val restTemplate: TestRestTemplate) {
 
     @Test
     fun `Assert blog page title, content and status code`() {
+        println(">> Assert blog page title, content and status code")
         val entity = restTemplate.getForEntity<String>("/")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
-        assertThat(entity.body).contains("<h1>Blog</h1>")
+        assertThat(entity.body).contains("<h1>Blog</h1>", "Reactor")
     }
 
     @Test
     fun `Assert article page title, content and status code`() {
-        println(">> TODO")
+        println(">> Assert article page titie, content and status code")
+        val title = "Reactor Aluminium has landed"
+        val entity = restTemplate.getForEntity<String>("/article/${title.toSlug()}")
+
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entity.body).contains(title, "Lorem ipsum", "dolor sitamet")
     }
 
     @AfterAll
